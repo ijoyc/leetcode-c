a047701c1e8fcf8dc4222f4ac1710e897b033be9
chenyizhuo.yizhuoc <chenyizhuo.yizhuoc@bytedance.com>
2020-12-03 16:34:12 +0800

    39_combination_sum.c add dp solution

--- medium/39_combination_sum.c
+++ medium/39_combination_sum.c
@@ -2,7 +2,7 @@
 * @Author: chenyizhuo.yizhuoc
 * @Date:   2020-12-02 21:49:33
 * @Last Modified by:   chenyizhuo.yizhuoc
-* @Last Modified time: 2020-12-03 14:59:31
+* @Last Modified time: 2020-12-03 16:33:58
 */
 
 #include <stdio.h>
@@ -18,10 +18,39 @@ typedef struct List {
 	int size;
 } List;
 
+typedef struct LList {
+	List **values;
+	int capacity;
+	int size;
+} LList;
+
 List *ListNew() {
 	List *list = (List *)malloc(sizeof(List));
 	list->size = 0;
 	list->head = NULL;
+	return list;
+}
+
+List *ListCopy(List *source) {
+	List *list = ListNew();
+	Node *p = source->head;
+	for (int i = 0; i < source->size; i++) {
+		ListAdd(list, p->value);
+		p = p->next;
+	}
+	return list;
+}
+
+
+LList *LListNew() {
+	int initialCapacity = 10;
+	LList *list = (LList *)malloc(sizeof(LList));
+	list->values = (List **)malloc(sizeof(List *) * initialCapacity);
+	list->size = 0;
+	list->capacity = initialCapacity;
+	for (int i = 0; i < initialCapacity; i++) {
+		list->values[i] = NULL;
+	}
 	return list;
 }
 
@@ -31,6 +60,16 @@ void ListAdd(List *list, int value) {
 	node->next = list->head;
 	list->head = node;
 	list->size++;
+}
+
+void LListAdd(LList *list, List *value) {
+	if (list->size == list->capacity) {
+		int newCapacity = list->capacity << 1;
+		list->values = (List **)realloc(list->values, sizeof(List *) * newCapacity);
+		list->capacity = newCapacity;
+	}
+
+	list->values[list->size++] = value;
 }
 
 void ListRemoveFirst(List *list) {
@@ -80,6 +119,47 @@ int **combinationSum(int *candidates, int candidatesSize, int target, int *retur
 	resolve(candidates, 0, candidatesSize, target, list, result, &size, columnSizes);
 
 	*returnSize = size;
+	*returnColumnSizes = columnSizes;
+	return result;
+}
+
+int **combinationSumDP(int *candidates, int candidatesSize, int target, int *returnSize, int **returnColumnSizes) {
+	// dp solution:
+	// dp[i][j] = all combinations using candidates 0 to i to sum to j
+	// base case: dp[0][0] = [[]]
+	// state transition: dp[i][j] = dp[i - 1][j] ++ dp[i][j - candidate]
+	// solution: dp[candidatesSize][target]
+
+
+	LList **lists = (LList **)malloc(sizeof(LList *) * (target + 1));
+	for (int i = 0; i <= target; i++) {
+		lists[i] = LListNew();
+	}
+	LListAdd(lists[0], ListNew());
+
+	for (int i = 0; i < candidatesSize; i++) {
+		int candidate = candidates[i];
+
+		for (int j = candidate; j <= target; j++) {
+			LList *list = lists[j - candidate];
+			for (int k = 0; k < list->size; k++) {
+                List *newNums = ListCopy(list->values[k]);
+				ListAdd(newNums, candidate);
+                LListAdd(lists[j], newNums);
+			}
+		}
+	}
+
+	LList *finalList = lists[target];
+	*returnSize = finalList->size;
+	
+	int **result = (int **)malloc(sizeof(int *) * finalList->size);
+	int *columnSizes = (int *)malloc(sizeof(int) * finalList->size);
+	for (int i = 0; i < finalList->size; i++) {
+		List *l = finalList->values[i];
+		result[i] = ListToArray(l);
+		columnSizes[i] = l->size;
+	}
 	*returnColumnSizes = columnSizes;
 	return result;
 }
