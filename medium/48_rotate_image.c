97b415d7037fa67a3b192e0cdaddf130219403dc
chenyizhuo.yizhuoc <chenyizhuo.yizhuoc@bytedance.com>
2021-08-23 11:01:42 +0800

    48_rotate_image

---
+++ medium/48_rotate_image.c
@@ -0,0 +1,12 @@
+
+void rotate(int **matrix, int matrixSize, int *matrixColSize) {
+    for (int i = 0; i < matrixSize / 2; i++) {
+        for (int j = i; j < matrixSize - i - 1; j++) {
+            int tmp = matrix[i][j];
+            matrix[i][j] = matrix[matrixSize - 1 - j][i];
+            matrix[matrixSize - 1 - j][i] = matrix[matrixSize - 1 - i][matrixSize - 1 - j];
+            matrix[matrixSize - 1 - i][matrixSize - 1 - j] = matrix[j][matrixSize - 1 - i];
+            matrix[j][matrixSize - 1 - i] = tmp;
+        }
+    }
+}
